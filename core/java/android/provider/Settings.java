9f858b88ddbb8a2d702211c77fc6adfbfce36f10
Ali B <abittin@gmail.com>
2021-01-02 21:14:45 +0000

    Allow to hide arrow for back gesture [1/2]
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14471,6 +14471,12 @@ public final class Settings {
         public static final String PULSE_BASS_HAPTICS = "pulse_bass_haptics";
 
         /**
+         * Whether to show or hide the arrow for back gesture
+         * @hide
+         */
+        public static final String BACK_GESTURE_ARROW = "back_gesture_arrow";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
