9e98bc6a29100e12eabfd0efdb8a4f2742bbbc1b
ezio84 <brabus84@gmail.com>
2022-01-16 13:20:36 +0400

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    Squashed:
    
        From: someone5678 <someone5678@users.noreply.github.com>
        Date: Sun, 30 Jun 2024 18:35:59 +0900
        Subject: fixup! Allow to suppress notifications sound/vibration if screen is ON [1/2]
    
        * Also apply it to NotificationAttentionHelper
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Tue, 23 Jul 2024 01:12:58 +0530
        Subject: fixup! Allow to suppress notifications
         sound/vibration if screen is ON [1/2]
    
        * We forgot to load settings on boot.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/notification/NotificationAttentionHelper.java
+++ services/core/java/com/android/server/notification/NotificationAttentionHelper.java
@@ -226,6 +226,8 @@ public final class NotificationAttentionHelper {
     private final PolitenessStrategy mStrategy;
     private int mCurrentWorkProfileId = UserHandle.USER_NULL;
 
+    private boolean mSoundVibScreenOn;
+
     public NotificationAttentionHelper(Context context, Object lock, LightsManager lightsManager,
             AccessibilityManager accessibilityManager, PackageManager packageManager,
             UserManager userManager, NotificationUsageStats usageStats,
@@ -379,6 +381,9 @@ public final class NotificationAttentionHelper {
 
         mContext.getContentResolver().registerContentObserver(
                 SettingsObserver.NOTIFICATION_LIGHT_PULSE_URI, false, mSettingsObserver,
+                UserHandle.USER_ALL);
+        mContext.getContentResolver().registerContentObserver(
+                SettingsObserver.NOTIFICATION_SOUND_VIB_SCREEN_ON_URI, false, mSettingsObserver,
                 UserHandle.USER_ALL);
 
         if (favoritesIncomingCallLights()) {
@@ -539,7 +544,8 @@ public final class NotificationAttentionHelper {
         }
 
         if (aboveThreshold && isNotificationForCurrentUser(record, signals)) {
-            if (mSystemReady && mAudioManager != null) {
+            boolean skipSound = mScreenOn && !mSoundVibScreenOn;
+            if (!skipSound && mSystemReady && mAudioManager != null) {
                 Uri soundUri = record.getSound();
                 hasValidSound = soundUri != null && !Uri.EMPTY.equals(soundUri);
                 VibrationEffect vibration = record.getVibration();
@@ -1937,6 +1943,8 @@ public final class NotificationAttentionHelper {
                 Settings.System.NOTIFICATION_COOLDOWN_ALL);
         private static final Uri NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED_URI =
                 Settings.System.getUriFor(Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED);
+        private static final Uri NOTIFICATION_SOUND_VIB_SCREEN_ON_URI =
+                Settings.System.getUriFor(Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON);
         public SettingsObserver() {
             super(null);
         }
@@ -1981,6 +1989,12 @@ public final class NotificationAttentionHelper {
                 synchronized (mLock) {
                     updateLightsLocked();
                 }
+            }
+            if (uri == null || NOTIFICATION_SOUND_VIB_SCREEN_ON_URI.equals(uri)) {
+                mSoundVibScreenOn = Settings.System.getIntForUser(
+                        mContext.getContentResolver(),
+                        Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, 1,
+                        UserHandle.USER_CURRENT) == 1;
             }
             if (Flags.politeNotifications()) {
                 if (uri == null || NOTIFICATION_COOLDOWN_ENABLED_URI.equals(uri)) {
