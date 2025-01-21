e6f24093ec625f42e3c69e6dc1a99b1447c395f7
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-21 21:18:25 +0900

    SystemUI: Add switch for compact HUN [1/2]
    * Now we can always enable it
    
    Change-Id: Ia27310b05df6b7ec222d91c0e46a068e4e8004e7
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/HeadsUpStyleProvider.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/HeadsUpStyleProvider.kt
@@ -16,6 +16,7 @@
 
 package com.android.systemui.statusbar.notification.row
 
+import android.os.SystemProperties
 import android.view.Display
 import com.android.app.displaylib.PerDisplayRepository
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent
@@ -36,7 +37,8 @@ constructor(
 ) : HeadsUpStyleProvider {
 
     override fun shouldApplyCompactStyle(displayId: Int): Boolean {
-        return android.app.Flags.alwaysShowMinimalHun() || isInImmersiveMode(displayId)
+        return isInImmersiveMode(displayId) ||
+            SystemProperties.getBoolean("persist.sys.compact_heads_up_notification.always_show", false)
     }
 
     private fun isInImmersiveMode(displayId: Int): Boolean {
