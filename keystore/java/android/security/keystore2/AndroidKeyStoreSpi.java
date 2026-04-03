1c8128e21befcab832a2d93bb4b5235f083d77aa
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-03 21:57:44 +0800

    core: Fixing stackoverflow in trickystore port
    Change-Id: I26c849f219c6ce0fe7239c55e0ac9ad3356bb912
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -121,6 +121,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     // Defined in RFC 8410.
     private static final String ED25519_OID = "1.3.101.112";
+    
+    private static final ThreadLocal<Boolean> sInHack = ThreadLocal.withInitial(() -> Boolean.FALSE);
 
     @Override
     public Key engineGetKey(String alias, char[] password) throws NoSuchAlgorithmException,
@@ -276,6 +278,10 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
         if (chain == null || chain.length == 0) {
             return chain;
         }
+        if (sInHack.get()) {
+            return chain;
+        }
+        sInHack.set(Boolean.TRUE);
         try {
             TrickyStoreService service = TrickyStoreService.getInstance();
             if (!service.hasKeyboxes()) {
@@ -295,6 +301,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
             }
         } catch (Exception e) {
             Log.e(TAG, "TrickyStore: Failed to hack certificate chain", e);
+        } finally {
+            sInHack.set(Boolean.FALSE);
         }
         return chain;
     }
