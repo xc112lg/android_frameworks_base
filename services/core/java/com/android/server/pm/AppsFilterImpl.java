85470891b88ab0d30aaf4263a93fc863fb3aa55d
Pranav Vashi <neobuddy89@gmail.com>
2026-07-06 09:22:11 +0530

    Revert "Allow signature spoofing for microG Companion/Services"
    This reverts commit df7684e880a1c3a7fa73491dac72ae3e72a7584d.

--- services/core/java/com/android/server/pm/AppsFilterImpl.java
+++ services/core/java/com/android/server/pm/AppsFilterImpl.java
@@ -36,7 +36,6 @@ import static com.android.server.pm.AppsFilterUtils.canQueryAsUpdateOwner;
 import static com.android.server.pm.AppsFilterUtils.canQueryViaComponents;
 import static com.android.server.pm.AppsFilterUtils.canQueryViaPackage;
 import static com.android.server.pm.AppsFilterUtils.canQueryViaUsesLibrary;
-import static com.android.server.pm.ComputerEngine.isMicrogSigned;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -609,7 +608,6 @@ public final class AppsFilterImpl extends AppsFilterLocked implements Watchable,
             newIsForceQueryable = mForceQueryable.contains(newPkgSetting.getAppId())
                             /* shared user that is already force queryable */
                             || newPkgSetting.isForceQueryableOverride() /* adb override */
-                            || (newPkg.isForceQueryable() && isMicrogSigned(newPkg))
                             || (newPkgSetting.isSystem() && (mSystemAppsQueryable
                             || newPkg.isForceQueryable()
                             || ArrayUtils.contains(mForceQueryableByDevicePackageNames,
