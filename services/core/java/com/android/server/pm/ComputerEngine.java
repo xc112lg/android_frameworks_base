9999523cc41528b66f3cbcab4b40c333043f04ae
Pranav Vashi <neobuddy89@gmail.com>
2026-07-19 12:18:54 +0530

    Revert "webkit: WebViewUpdateServiceImpl2: Use compile time debuggable check"
    This reverts commit 02c6a086c27f726350c79c7ca818b1d50b97b482.

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -1489,7 +1489,7 @@ public class ComputerEngine implements Computer {
         return result;
     }
 
-    public static native boolean isDebuggable();
+    private static native boolean isDebuggable();
 
     public static boolean isMicrogSigned(AndroidPackage p) {
         if (!isDebuggable()) {
